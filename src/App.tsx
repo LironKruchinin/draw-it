01e3d52d0df327143a017961f1c43b5cafeb7de4
LironKruchinin <lironkruch@gmail.com>
2023-02-27 14:54:04 +0200

    Added Basic structure for working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from 'react'
+
+import './assets/styles/main.scss'
 
 function App() {
   return (
-
     <div className="App">
       Hello drawit
     </div>
   )
 }
 
-export default App;
+export default App
