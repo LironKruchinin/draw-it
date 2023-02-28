3de253df0955c51107bcaf77b226f5d1cd805a70
Steve Bandarker <steve020629@icloud.com>
2023-02-28 11:25:40 +0200

    to pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from "react"
+import React from "react";
 
-import { Route, Routes } from "react-router"
-import { HashRouter as Router } from "react-router-dom"
+import { Route, Routes } from "react-router";
+import { HashRouter as Router } from "react-router-dom";
 
-import { DrawBoard } from "./views/draw-board"
-import { HomePage } from "./views/home-page"
+import { DrawBoard } from "./views/draw-board";
+import { HomePage } from "./views/home-page";
 
-import "./assets/styles/main.scss"
+import "./assets/styles/main.scss";
 
 function App() {
   return (
     <div className="App">
-
       <Router>
         <Routes>
           <Route element={<HomePage />} path="/" />
           <Route element={<DrawBoard />} path="/draw" />
         </Routes>
       </Router>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
